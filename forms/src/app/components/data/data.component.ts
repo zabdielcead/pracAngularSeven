3b457d13290ebaef3eb05426b9024f6f152be533
zabdielcead <zabdiel.cead@gmail.com>
2019-01-04 14:42:54 -0600

    suben cambios de data y forms

--- forms/src/app/components/data/data.component.ts
+++ forms/src/app/components/data/data.component.ts
@@ -9,13 +9,25 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class DataComponent implements OnInit {
   forma: FormGroup;
+  usuario: any = {
+    nombrecompleto: {
+      nombre: 'carlos',
+      apellido: 'arteaga'
+    },
+    correo: 'zab@hotmail.com'
+  };
 
   constructor() {
+    console.log(this.usuario);
     this.forma = new FormGroup({
-      'nombre': new FormControl('CARLOS', Validators.required),
-      'apellido': new FormControl('', Validators.required),
-      'correo': new FormControl('' , [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl('', Validators.required)
+      }),
+       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
+    this.forma.setValue(this.usuario); // cargar el usuario en el formulario
    }
 
   ngOnInit() {
@@ -24,6 +36,9 @@ export class DataComponent implements OnInit {
   guardarCambios() {
     console.log( this.forma.value );
     console.log( this.forma );
+
+    // pristine es el formulario recien cargado
+    this.forma.reset(this.usuario); // lo regresa a su estaod pristine
   }
 
 }
