ca9e55f709981f318abdc80979364e552a5eec0f
zabdielcead <zabdiel.cead@gmail.com>
2019-01-07 18:21:39 -0600

    suben cambios y validaciones

--- forms/src/app/components/data/data.component.ts
+++ forms/src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 
 @Component({
@@ -14,7 +14,8 @@ export class DataComponent implements OnInit {
       nombre: 'carlos',
       apellido: 'arteaga'
     },
-    correo: 'zab@hotmail.com'
+    correo: 'zab@hotmail.com',
+    // pasatiempos: ['correr', 'dormir', 'comer']
   };
 
   constructor() {
@@ -23,11 +24,20 @@ export class DataComponent implements OnInit {
 
       'nombrecompleto': new FormGroup({
         'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
-        'apellido': new FormControl('', Validators.required)
+        'apellido': new FormControl('', [Validators.required, this.noHerrera]) // noHerrera va sin parentesis ojo
       }),
-       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+       'pasatiempos': new FormArray([
+         new FormControl('Correr', Validators.required)
+       ]),
+       'password1': new FormControl( '', Validators.required ),
+       'password2': new FormControl(  )
     });
-    this.forma.setValue(this.usuario); // cargar el usuario en el formulario
+   // this.forma.setValue(this.usuario); // cargar el usuario en el formulario
+     this.forma.controls['password2'].setValidators([
+        Validators.required,
+        this.noIgual.bind(this.forma)  // bind especificar un valor a this por defecto
+     ]);
    }
 
   ngOnInit() {
@@ -41,4 +51,29 @@ export class DataComponent implements OnInit {
     this.forma.reset(this.usuario); // lo regresa a su estaod pristine
   }
 
+  agregarPasatiempo() {
+    (<FormArray> this.forma.controls['pasatiempos']).push(
+      new FormControl('Dormir', Validators.required)
+    );
+  }
+
+  noHerrera( control: FormControl ): {[s: string]: boolean} {
+    if ( control.value === 'herrera' ) {
+      return {
+        noherrera: true
+      };
+    }
+    return null;
+  }
+  noIgual( control: FormControl ): {[s: string]: boolean} {
+    console.log(this);
+    let formas: any = this;
+
+    if ( control.value !== formas.controls['password1'].value ) {
+      return {
+        noiguales: true
+      };
+    }
+    return null;
+  }
 }
